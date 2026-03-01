783db60a49212d2e6f80e955767f626221caae97
yossi_16 <ylerer770@gmail.com>
2026-03-01 16:24:32 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import './App.css'
+import Shop from './pages/Shop'
+import Card from './components/Card'
+
+function App() {
+
+
+  return (
+    <>
+    <Shop />
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Card from '../components/Card'
+import data from '../../public/data.json'
+
+export default function Shop() {
+
+    
+    
+  return (
+    <div>
+        {data.map((item) => {
+            return (
+                <Card 
+                key={item.id}
+                id={item.id} 
+                name={item.name} 
+                price={item.price}
+                category={item.category}
+                description={item.description}
+                image={image}  />
+            )
+        })}
+    
+    </div>
+  )
+}
